bda4c88c0f5f244c5aa2b1cc3b23d7b99d00cb7a
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-09-24 11:06:16 +0300

    Update TodoModel.js

--- models/TodoModel.js
+++ models/TodoModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
     Task: { type: String, required: true }, // Ensure Task is a required field
-    order: { type: Number, required: false } // Add order field
+    order: { type: Number, required: false }, // Add order field
+    done: { type: Boolean, default: false } // Add done field
 }, {
     timestamps: true
 });
