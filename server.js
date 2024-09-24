98b0e7411df49ffa9cee2620514c237e0cdc8022
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-09-24 09:56:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin:'https://todo-client-git-main-mak-pentaroks-projects.vercel.app/',
+    origin:'https://todo-client-git-main-mak-pentaroks-projects.vercel.app',
     methods:["GET,POST,PUT,DELETE"],
     credentials:true
 }));
